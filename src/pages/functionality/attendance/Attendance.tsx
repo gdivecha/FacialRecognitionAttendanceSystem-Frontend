ac12dd86242ed018a7d0d0a95af384714facb5c0
Gaurav Divecha <gauravdivecha@Gauravs-MacBook-Pro.local>
2024-11-12 20:18:56 -0500

    Added the add new student functionality to the system

--- src/pages/functionality/attendance/Attendance.tsx
+++ src/pages/functionality/attendance/Attendance.tsx
@@ -1,11 +1,17 @@
-import React from "react";
+import { Paper, Typography } from "@mui/material";
 
 function Attendance() {
 
     return (
-        <p>
-            This is the attendance page
-        </p>
+        <Paper sx={{ p: 2, width: '100%' }}>
+            <Typography variant="h5" fontWeight={1000} gutterBottom>
+                Attendance Tracking 
+            </Typography>
+            <Typography variant="body1" gutterBottom>
+                Record attendance for your students here by capturing live snapshots of their faces
+            </Typography>
+
+        </Paper>
     );
 }
 
